08b0740fb17a4abe7712fad9640ccc49a46ad179
azeem1a <110708546+azeem1a@users.noreply.github.com>
2023-01-21 08:57:58 -0800

    Add files via upload

---
+++ WithoutApi/src/test/java/com/example/WithoutApi/WithoutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WithoutApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithoutApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
